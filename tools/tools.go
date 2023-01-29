b34d3ec35f7df0b3c74c78576f05ea1f87e31fad
francisco <f.s.f.b2001@gmail.com>
2023-01-29 17:38:25 +0000

    add sql query generator and pgx driver

--- tools/tools.go
+++ tools/tools.go
@@ -6,4 +6,7 @@ package tools
 import (
 	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"       // Database Migrations
 	_ "github.com/golang-migrate/migrate/v4/database/postgres" // Database Migrations driver
+
+	_ "github.com/jackc/pgx/v5"             // PostgresSQL db driver
+	_ "github.com/kyleconroy/sqlc/cmd/sqlc" // Type-Safe SQL generator
 )
