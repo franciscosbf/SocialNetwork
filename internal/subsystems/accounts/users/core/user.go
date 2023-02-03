2b3a3819fc7ebc90e9886d00215715553015c97a
francisco <f.s.f.b2001@gmail.com>
2023-02-03 17:43:16 +0000

    add initial user repository specification

---
+++ internal/subsystems/accounts/users/core/user.go
@@ -0,0 +1,112 @@
+package core
+
+import (
+	"fmt"
+)
+
+// UserPhone represents an optional user
+// phone containing its country code and number
+type UserPhone struct {
+	Prefix int
+	Number int
+}
+
+func (p *UserPhone) String() string {
+	return fmt.Sprintf(
+		"UserPhone[Prefix: %v, Number: %v]",
+		p.Prefix, p.Number)
+}
+
+// UserName represents the name elements
+type UserName struct {
+	First   string
+	Middle  string
+	Surname string
+}
+
+func (un *UserName) String() string {
+	return fmt.Sprintf(
+		"UserName[First: %v, Middle: %v, Surname: %v]",
+		un.First, un.Middle, un.Surname)
+}
+
+// UserRegistration represents the
+// required elements to register a user
+type UserRegistration struct {
+	Username string
+	Email    string
+	Password string
+
+	Phone *UserPhone
+
+	Name *UserName
+}
+
+func (ur *UserRegistration) String() string {
+	return fmt.Sprintf(
+		"UserRegistration[Username: %v, Email: %v, Password: %v, Phone: %v, Name: %v]",
+		ur.Username, ur.Email, ur.Password, ur.Phone, ur.Name)
+}
+
+// UserLocation represents an optional location
+// where the user is currently living. Expects
+// to follow the SRID 4326 (WGS84 long/lat)
+type UserLocation struct {
+	Longitude float64
+	Latitude  float64
+}
+
+func (ul *UserLocation) String() string {
+	return fmt.Sprintf(
+		"UserLocation[Longitude: %v, Latitude: %v]",
+		ul.Longitude, ul.Latitude)
+}
+
+// UserInfo represents all info
+// about a registered user
+type UserInfo struct {
+	Username string
+	Email    string
+	Password string
+
+	Phone *UserPhone
+
+	Name *UserName
+
+	Location *UserLocation
+}
+
+func (uf *UserInfo) String() string {
+	return fmt.Sprintf(
+		"UserRegistration[Username: %v, Email: %v, Password: %v, Phone: %v, Name: %v, Location: %v]",
+		uf.Username, uf.Email, uf.Password, uf.Phone, uf.Name, uf.Location)
+}
+
+// UsersRepository represents all user operations involved.
+// Each one may return an error if something went wrong
+type UsersRepository interface {
+	// RegisterUser Inserts a new user. UserPhone
+	// and UserName.Middle are optional fields
+	RegisterUser(user *UserRegistration) error
+
+	// DeleteUser removes a user from the system
+	DeleteUser(username string) error
+
+	GetEmail(username string) (string, error)
+	SetEmail(username, email string) error
+
+	MatchesPassword(username, password string) (bool, error)
+	SetPassword(username, password string) error
+
+	GetPhone(username string) (*UserPhone, error)
+	SetPhone(username string, phone *UserPhone) error
+
+	GetName(username string) (*UserName, error)
+	SetName(username string, name *UserName) error
+
+	GetDescription(username string) (string, error)
+	SetDescription(username, description string)
+
+	GetLocation(username string) (*UserLocation, error)
+	SetLocation(username string, location *UserLocation) error
+}
