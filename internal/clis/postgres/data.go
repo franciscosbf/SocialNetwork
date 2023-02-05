8ed78ee07714fd8a70af025625fc9398ab426505
francisco <f.s.f.b2001@gmail.com>
2023-02-05 01:37:40 +0000

    add missing license boilerplate (shameful as always...)

--- internal/clis/postgres/data.go
+++ internal/clis/postgres/data.go
@@ -1,8 +1,24 @@
+/*
+Copyright 2023 Francisco Simões Braço-Forte
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package postgres
 
-// Connection contains required connection data 
-// and optional pool configuration. Fields with 
-// zero values and empty strings are ignored 
+// Connection contains required connection data
+// and optional pool configuration. Fields with
+// zero values and empty strings are ignored
 type Connection struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
