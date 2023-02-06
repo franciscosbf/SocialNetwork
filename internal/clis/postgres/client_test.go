885e10e92b8a17288f02f2397609f37c665566a3
francisco <f.s.f.b2001@gmail.com>
2023-02-06 16:40:36 +0000

    add func that unsets all config vars

--- internal/clis/postgres/client_test.go
+++ internal/clis/postgres/client_test.go
@@ -3,12 +3,20 @@ package postgres
 import (
 	"github.com/franciscosbf/micro-dwarf/internal/envvars"
 	"github.com/franciscosbf/micro-dwarf/internal/envvars/providers"
+	"os"
 	"testing"
 )
+
+func unsetConfigVars() {
+	for _, pair := range confVars {
+		_ = os.Unsetenv(pair.varName)
+	}
+}
 
 func TestValidConnection(t *testing.T) {
 	envProvider := providers.NewEnvVariables()
 	conf := envvars.NewConfig(envProvider)
+	defer unsetConfigVars()
 
 	if cli, err := NewPostgresCli(conf); err != nil {
 		t.Errorf("Unexpecting error: %v", err)
